75e9d76b66b4a09e87d11eeff1e3f2d7d4896691
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-10-21 19:32:28 -0400

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Cars
+    {
+        public string carManufacturer;
+        public int modelDate;
+        public string carColor;
+
+        public Cars(string manufacturer, string color, int date)
+        {
+            carManufacturer = manufacturer;
+            carColor = color;
+            modelDate = date;
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Cars toyotaTacoma = new Cars("Toyota", "silver", 2018);
+            Console.WriteLine("My car is a " + toyotaTacoma.carManufacturer + " and was made in " + toyotaTacoma.modelDate + ". It's color is " + toyotaTacoma.carColor + ".");
+            
+        }
+    }
+}
